7a59f0cc3d8a1c07dd3ac1f73635ddad8d2e38aa
CoderWZW <122643224@qq.com>
2018-08-08 20:35:32 +0800

    update the FileIO,but the implement of attack algorithm didn't.

--- main/attackmain.py
+++ main/attackmain.py
@@ -8,22 +8,26 @@ Created on Fri Jul 27 17:00:00 2018
 import sys
 from os.path import abspath
 
-
+import random
 import time
 
 sys.path.append('../')
 from baseclass.shillingAttack.Attack import Attack
 from algorithm.shillingAttack.randomAttack import RandomAttack
+from algorithm.shillingAttack.bandwagonAttack import BandWagonAttack
+from algorithm.shillingAttack.averageAttack import AverageAttack
 if __name__ == "__main__":
     print ('Welcome to use Dlib!')
-    print ("1.RandomAttack")
+    print ("1.RandomAttack   2.AverageAttack   3.BandwagonAttack")
     print ("You have choose the Data Attack:),please choose the attack way:")
     attack = int(input())
     s = time.time()
     if attack == 1:
-        dla = RandomAttack('../config/attack_config.conf')
+        RandomAttack('../config/attack_config.conf')
     elif attack == 2:
-        print ('2')
+        AverageAttack('../config/attack_config.conf')
+    elif attack == 3:
+        BandWagonAttack('../config/attack_config.conf')
     else:
         print ('Error num!')
         exit(-1)
